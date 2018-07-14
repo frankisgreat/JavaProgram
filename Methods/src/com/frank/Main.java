3eb750387be65e381bd1594eb83e9ccee907ee1a
frankisgreat <fengwanglosangeles@gmail.com>
2018-07-13 17:58:42 -0700

    7/13/2018
    Done with Method

--- Methods/src/com/frank/Main.java
+++ Methods/src/com/frank/Main.java
@@ -1,69 +1,80 @@
 package com.frank;
 
+import javax.print.StreamPrintService;
+import javax.swing.text.Position;
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("THIIS IS A METHOD!");
 
-        // calculateScore(true, 800, 5, 100);
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("The Final Score is: " + highScore);
-        // An Alternative approach:
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your final score was " + highScore);
         highScore = calculateScore(true, 10000, 8, 200);
-        System.out.println("The Final Score is: " + highScore);
+        System.out.println("Your final score was " + highScore);
 
-        // Create a method called displayHighScorePosition
-        // it should have a players name as a parameter, and a 2nd parameter as a position in the high score table
-        // you should display the players name along with a message like " managed to get into position" and the
-        // position they got and a further message " on the high score table",
-        //
-        // Create a  2nd method called calculateHighScorePosition
-        // it should be sent one argument only, the player score
-        // it should return a int
-        // the return data should be
-        // 1 if the score is >1000
-        // 2 if the score is >500 and < 1000
-        // 3 if the score is >100 and < 500
-        // 4 in all other cases
-        // call both methods and display the results of the following
-        // a score of 1500, 900, 400, and 50
-           displayHighScorePosition("Frank",800 );
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tom", highScorePosition);
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Tom", highScorePosition);
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Tom", highScorePosition);
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Tom", highScorePosition);
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Louis", highScorePosition);
+
     }
 
-    public static void displayHighScorePosition (String playerName, int calculateHighScorePosition ){
-        int playerPosition = calculateHighScorePosition(calculateHighScorePosition);
-        System.out.println( playerName + " managed to get into position " + calculateHighScorePosition + " on the high score table.");
-    }
-    public static int calculateHighScorePosition( int score){
-
-        if (score > 1000 ){
-
-            return 1;
-        }
-        else if(score > 500 && score <1000){
-            return 2;
-        }
-        else if(score >100 && score < 500){
-            return 3;
-        }else{
-            return 4;
-        }
-    }
-    //Creating Method (you can't create method inside a method
-    // Void means don't return anything
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             return finalScore;
-        } else {
-            return -1;
         }
+        return -1;
 
+    }
+    // Create a method called displayHighScorePosition
+// it should a players name as a parameter, and a 2nd parameter as a position in the high score table
+// You should display the players name along with a message like " managed to get into position " and the
+// position they got and a further message " on the high score table".
+//
+// Create a 2nd method called calculateHighScorePosition
+// it should be sent one argument only, the player score
+// it should return an in
+// the return data should be
+// 1 if the score is >=1000
+// 2 if the score is >=500 and < 1000
+// 3 if the score is >=100 and < 500
+// 4 in all other cases
+// call both methods and display the results of the following
+// a score of 1500, 900, 400 and 50
+//
+
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+//        if (score >= 1000) {
+//            return 1;
+//        } else if (score >= 500 ) {
+//            return 2;
+//        } else if (score >= 100 ) {
+//            return 3;
+//        }
+//        return 4;
+        int position = 4;
+
+        if(score >= 1000 ){
+            position =1;
+        }else if (score >=500){
+            position =2;
+        }else if (score >=100){
+            position =3;
+        }
+        return position;
     }
 }
